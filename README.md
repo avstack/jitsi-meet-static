0ab9fc7d7d74ec6296446c32b8599602a7a062f6
Jasper Hugo <jasper@jasperhugo.com>
2021-11-22 12:51:55 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,87 @@
+# jitsi-meet-static: Deploy your Jitsi Meet frontend as a static site
+
+This repository contains a Python script that can compile a Jitsi Meet frontend into static HTML/CSS/JS that can be deployed to any static site host.
+
+This makes it easier to decouple Jitsi Meet's static frontend from the backend components, giving more flexibility for large deployments, and making it easier to use hosted services like [AVStack](https://www.avstack.io/).
+
+This script should work with any relatively recent Jitsi Meet frontend version, and with custom frontends based on any recent Jitsi Meet frontend version. Some modifications may be needed for custom frontends.
+
+## Usage
+
+```
+# ./jitsi-meet-static.py --help
+usage: jitsi-meet-static.py [-h] --input INPUT --output OUTPUT [--config-url CONFIG_URL] [--interface-config-url INTERFACE_CONFIG_URL]
+
+Compile the Jitsi Meet frontend to a static site
+
+options:
+  -h, --help            show this help message and exit
+  --input INPUT         the path to the checked-out Jitsi Meet source code that will be built
+  --output OUTPUT       the destination directory for the compiled static site (which should not already exist)
+  --config-url CONFIG_URL
+                        a config.js URL, if it should be loaded dynamically rather than baked in
+  --interface-config-url INTERFACE_CONFIG_URL
+                        an interface_config.js URL, if it should be loaded dynamically rather than baked in
+```
+
+## Requirements
+
+* Python 3.
+* Node.js and NPM: different Jitsi Meet versions have varying Node.js and NPM requirements for building. The current development code requires Node.js 16 and NPM 8. Previous stable versions required Node.js 14 and NPM 7.
+
+## Example
+
+Build the latest Jitsi Meet stable frontend as a static site, loading its configuration dynamically from an AVStack stack called `teststack` so that it will use that stack as its backend:
+
+**You will need to change the stack name from `teststack` to the name of your own stack for the compiled frontend to work!**) You can get a free 5-user stack at [AVStack](https://www.avstack.io/), or configure your own self-hosted backend instead.
+
+```
+# git clone https://github.com/jitsi/jitsi-meet.git
+# cd jitsi-meet
+# git checkout stable/jitsi-meet_6433
+# cd ..
+
+# git clone https://github.com/avstack/jitsi-meet-static.git
+# cd jitsi-meet-static
+# ./jitsi-meet-static.py --input ../jitsi-meet --output ../out --config-url https://teststack.onavstack.net/config.js --interface-config-url https://teststack.onavstack.net/interface_config.js
+# cd ..
+```
+
+After running these commands, you have a compiled static Jitsi Meet frontend in the `out` folder. You can upload the content of this folder to any static site host that supports the requirements below.
+
+## Hosting requirements
+
+* HTTPS. Browsers will not allow WebRTC on non-secure origins.
+* Must be able to redirect non-existent paths matching the conference name format to /index.html. For example, using CloudFront Functions:
+  ```
+  function handler(event) {
+    var request = event.request;
+    if (request.uri.match(/^\/[A-Za-z0-9\/_-]+$/)) {
+      request.uri = "/index.html";
+    }
+    return request;
+  }
+  ```
+
+## Support
+
+AVStack customers are welcome to contact your AVStack representative for support with this script. Otherwise, please open an issue or pull request!
+
+## License
+
+`jitsi-meet-static` is licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Any kinds of contributions are welcome as a pull request.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in these crates by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+
+## Acknowledgements
+
+`jitsi-meet-static` development is sponsored by [AVStack](https://www.avstack.io/). We provide globally-distributed, scalable, managed Jitsi Meet backends.
---
+++ jitsi-meet-static.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import json
+import os
+import re
+import shutil
+import subprocess
+import tempfile
+
+parser = argparse.ArgumentParser(description="Compile the Jitsi Meet frontend to a static site")
+parser.add_argument('--input', type=str, required=True, help='the path to the checked-out Jitsi Meet source code that will be built')
+parser.add_argument('--output', type=str, required=True, help='the destination directory for the compiled static site (which should not already exist)')
+parser.add_argument('--config-url', type=str, help='a config.js URL, if it should be loaded dynamically rather than baked in')
+parser.add_argument('--interface-config-url', type=str, help='an interface_config.js URL, if it should be loaded dynamically rather than baked in')
+args = parser.parse_args()
+
+# Remove checksums from Git dependencies in the NPM package lockfile to prevent spurious build errors
+# on non-x86 architectures.
+#
+# The git dependencies specify a commit hash, so Git's hashing is already protecting the integrity.
+# NPM's checksum algorithm is broken: it compresses the checked-out Git repository before hashing it,
+# which results in a different checksum on different architectures.
+#
+# https://github.com/npm/cli/issues/2846 
+with open(os.path.join(args.input, "package-lock.json")) as f:
+  lockfile = json.load(f)
+modified = False
+for package in lockfile["packages"].values():
+  if "resolved" in package and "integrity" in package and package["resolved"].startswith("git+ssh://"):
+    del package["integrity"]
+    modified = True
+if modified:
+  with open(os.path.join(args.input, "package-lock.json"), "w") as f:
+    json.dump(lockfile, f)
+
+subprocess.run("npm --loglevel=error install --no-audit --no-fund", shell=True, cwd=args.input, check=True)
+try:
+  subprocess.run("./node_modules/.bin/webpack -p", shell=True, cwd=args.input, check=True)
+except:
+  print("Re-running webpack without -p...")
+  subprocess.run("./node_modules/.bin/webpack", shell=True, cwd=args.input, check=True)
+subprocess.run("make deploy-init deploy-appbundle deploy-rnnoise-binary deploy-tflite deploy-meet-models deploy-lib-jitsi-meet deploy-libflac deploy-olm deploy-css", shell=True, cwd=args.input, check=True)
+
+os.mkdir(args.output)
+for subdir in ["libs", "fonts", "images", "lang", "sounds", "static"]:
+  shutil.copytree(os.path.join(args.input, subdir), os.path.join(args.output, subdir))
+
+shutil.copy2(os.path.join(args.input, "resources", "robots.txt"), os.path.join(args.output, "robots.txt"))
+shutil.copy2(os.path.join(args.input, "libs", "external_api.min.js"), os.path.join(args.output, "external_api.js"))
+
+with open(os.path.join(args.input, "lang", "languages.json")) as f:
+  languages = json.load(f)
+  for language in languages.keys():
+    language_filename = os.path.join(args.input, "node_modules", "i18n-iso-countries", "langs", f"{language}.json")
+    if os.path.isfile(language_filename):
+      shutil.copy2(language_filename, os.path.join(args.output, "lang", f"countries-{language}.json"))
+
+if os.path.isfile(os.path.join(args.input, "pwa-worker.js")):
+  shutil.copy2(os.path.join(args.input, "pwa-worker.js"), os.path.join(args.output, "pwa-worker.js"))
+
+os.mkdir(os.path.join(args.output, "css"))
+for file in glob.glob("*.css", root_dir=os.path.join(args.input, "css")):
+  shutil.copy2(os.path.join(args.input, "css", file), os.path.join(args.output, "css", file))
+
+for file in glob.glob("*.html", root_dir=args.input) + glob.glob("static/*.html", root_dir=args.input):
+  with open(os.path.join(args.input, file)) as in_f:
+    content = in_f.read()
+    if args.config_url is not None:
+      content = content.replace('<script><!--#include virtual="/config.js" --></script><!-- adapt to your needs, i.e. set hosts and bosh path -->', f'<script src="{args.config_url}"></script>')
+    if args.interface_config_url is not None:
+      content = content.replace('<script><!--#include virtual="/interface_config.js" --></script>', f'<script src="{args.interface_config_url}"></script>')
+    content = re.sub(r'<!--#include\svirtual="([^"]+)"\s-->', lambda match: open(os.path.join(args.input, match[1].lstrip("/"))).read(), content)
+    with open(os.path.join(args.output, file), "w") as out_f:
+      out_f.write(content)
